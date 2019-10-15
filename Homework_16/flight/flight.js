86a83dad8f74dc273aa111b1dea989a5296c16e6
Ozonnie <alex.protashyk@gmail.com>
2019-10-15 14:21:22 +0300

    v2

--- Homework_16/flight/flight.js
+++ Homework_16/flight/flight.js
@@ -1,19 +1,19 @@
 let number, origin, destination;
 
-module.exports.setNumber = function(num){
+module.exports.setNumber = function (num) {
     number = num;
 };
 
-module.exports.setOrigin = function(o){
+module.exports.setOrigin = function (o) {
     origin = o;
 };
 
-module.exports.setDestination = function(d){
+module.exports.setDestination = function (d) {
     destination = d;
 };
 
-module.exports.getInfo = function(){
-    return{
+module.exports.getInfo = function () {
+    return {
         number: number,
         origin: origin,
         destination: destination
